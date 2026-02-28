11b4af331fdaf0eb75493e14f490b763be9478f9
DeniseRiva <denise.riva@stud.sfgz.ch>
2026-02-28 14:53:30 +0100

    ende unterricht tag

---
+++ script.js
@@ -0,0 +1,23 @@
+// Variables
+const name = "JavaScript";
+const version = 1.0;
+
+// Function
+function greet(userName) {
+    return `Hello, ${userName}!`;
+}
+
+// Object
+const user = {
+    name: "John",
+    age: 25,
+    email: "john@example.com"
+};
+
+// Array
+const numbers = [1, 2, 3, 4, 5];
+
+// Console output
+console.log(greet(user.name));
+console.log("Numbers:", numbers);
+console.log("User:", user);
